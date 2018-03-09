ccf9d3efad0ee019b1d1da66e44b767f332f00c5
jaekwang <jeakwang@flitto.com>
2018-03-09 23:26:49 +0900

    modify code

--- multi-search/src/containers/FilterContainers.js
+++ multi-search/src/containers/FilterContainers.js
@@ -1,57 +1,52 @@
 import React, { Component } from 'react';
-import { Header, InputFields, List } from '../components'
+import { InputFields, List } from '../components'
 
 class FilterContainers extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      originList: [],
-      filteredList: []
+      list: []
     };
     this.handleChange = this.handleChange.bind(this);
   }
+
+  originList = [];
 
   componentDidMount() {
     const API_URL = "https://raw.githubusercontent.com/mledoze/countries/master/countries.json";
     fetch(API_URL)
       .then((resp) => resp.json())
       .then((list) => {
-        this.setState({
-          originList: list,
-          filteredList: list 
-        });
+        this.originList = list; 
+        this.setState({ list });
       })
   }
 
   handleChange(e) {
     const { name, value } = e.target;
     const filter = Object.assign({}, {[name]:value})
-    this.filteringFactory(filter);
+    const list = this.filteringFactory(filter);
+    this.setState({ list })
   }
 
   filteringFactory(filter) {
-    const { originList } = this.state
     const { country = '', capital = '', region = '', subregion = '' } = filter;
-     
-    let filteredList = originList.filter((item, i) => {
+    let list = this.originList.filter((item, i) => {
       let countryFilter = item.name.official.match(new RegExp(country), 'gi') ;
       let capitalFilter = item.capital.join('').match(new RegExp(capital), 'gi');
       let regionFilter = item.region.match(new RegExp(region), 'gi');
       let subregionFilter = item.subregion.match(new RegExp(subregion), 'gi'); 
-      
-      if (countryFilter && capitalFilter && regionFilter && subregionFilter) {
-        return item;
-      }
+      return (countryFilter && capitalFilter && regionFilter && subregionFilter) ? item : '';
     })
-    this.setState({ filteredList });
+    return list;
   }
 
   render() {
     return (
       <section>
         <InputFields handleChange={this.handleChange} />
-        <List list={this.state.filteredList}/>
+        <List list={this.state.list}/>
       </section>
     );
   }
